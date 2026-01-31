b87b7e15a87f93d0c7a3854c60fd46f5813928eb
rbillings2022 <rbillings2022@fau.edu>
2026-01-31 09:10:12 -0500

    Adding more Slicing

--- slicing.py
+++ slicing.py
@@ -1,7 +1,7 @@
 #String slicing is specificing char of a string with a start index - end index (Not including the number)
 myname = 'Bob Smitten'
 dude = ' is a dude'
-print(len(myname))
+print(len(myname)) 
 print(myname[0]) #Starting letter of the string
 print(myname[1:]) #Slice from second letter to end.
 print("dude " + myname[0:]) #concatenate two strings from start
@@ -11,3 +11,5 @@ print(myname[:-3])
 print(myname[0:1])
 print(myname[:1]) #End index 0 displayed
 print(dude[1:3] + myname[1:3])
+
+#More practice with slicing using epedition 33
